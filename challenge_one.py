32ad16637601820893568450724bae8840beb98c
John <johntaykimani@gmail.com>
2024-03-03 14:47:16 +0300

    Feat: Defined function for the solution.

---
+++ challenge_one.py
@@ -0,0 +1,6 @@
+# Given an array A of N integers, the function solution returns
+# the minimum number of moves needed to end up with exactly 10 bricks in every box.
+# If this is not possible, the function should return −1.
+
+def solution(A):
+    pass
