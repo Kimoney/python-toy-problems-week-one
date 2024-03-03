8a47a1e05111260c574f8694245155ac7d13db18
John <johntaykimani@gmail.com>
2024-03-03 22:27:18 +0300

    feat: Validate argument is an array  else raise an error.

--- challenge_two.py
+++ challenge_two.py
@@ -1,7 +1,11 @@
 #  Write a function given an array A consisting of N integers, returns the maximum sum of two numbers whose digits add up to an equal sum.
 #  If there are no two numbers whose digits have an equal sum, the function should return -1.
 def solution(A):
-    pass
+    # Validate the data is an Array
+    if type(A) == list:
+        pass
+    else:
+        raise ValueError("A has to be a list.")
 
 # Test Cases
 print(solution([51, 71, 17, 42]))
