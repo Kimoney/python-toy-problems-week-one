ce90ba969e7c4b288099270c1fa5a1a6f9174b4b
John <johntaykimani@gmail.com>
2024-03-03 14:59:30 +0300

    Feat: Add logic to ensure we end up with exactly 10 bricks in every box. If this is not possible, the function should return −1.

--- challenge_one.py
+++ challenge_one.py
@@ -3,4 +3,10 @@
 # If this is not possible, the function should return −1.
 
 def solution(A):
-    pass
+    # Check whether the sum of all integers is a multiple of 10 to ensure we end up with exactly 10 bricks in every box
+    if (type(A) == list and sum(A) % 10 == 0):
+        print("okay")
+    else:
+        print(-1)
+
+solution([7, 15, 10, 8])
