eb8dcdad526853b2a7c10a46e540655ba4c2fecd
John <johntaykimani@gmail.com>
2024-03-03 22:21:50 +0300

    feat: Defined function for challenge 2.

---
+++ challenge_two.py
@@ -0,0 +1,11 @@
+#  Write a function given an array A consisting of N integers, returns the maximum sum of two numbers whose digits add up to an equal sum.
+#  If there are no two numbers whose digits have an equal sum, the function should return -1.
+def solution(A):
+    pass
+
+# Test Cases
+print(solution([51, 71, 17, 42]))
+print(solution([42, 33, 60]))
+print(solution([51, 32, 43]))
+print(solution("Just a string"))
+print(solution(1234))
