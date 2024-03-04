ad20d00af87d25c0d254fe6a543e7cd7141feb4c
John <johntaykimani@gmail.com>
2024-03-04 10:50:44 +0300

    feat: Validate the input N is an integer between 1 and 200000, else raise an error

--- challenge_three.py
+++ challenge_three.py
@@ -3,8 +3,12 @@
 # in which each letter occurs an equal number of times.
 
 def solution(N):
-    pass
-
+    # Validate N is an innteger, and is bwtween 1 and 200,000
+    if type(N) == int and 1 <= N <= 200000:
+        print("truthy")
+    else:
+        raise ValueError("'N' has to be an integer between 1 and 200000")
+    
 # Test Cases
 print(solution(3))
 print(solution(5))
