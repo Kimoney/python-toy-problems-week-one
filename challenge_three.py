d89df54fbf34724fd30ab1e9f5e51154684cfee5
John <johntaykimani@gmail.com>
2024-03-04 11:20:25 +0300

    chore: Import string module

--- challenge_three.py
+++ challenge_three.py
@@ -1,11 +1,14 @@
 # Write a function solution that, given an integer N, returns a string of length N containing
 # as many different lower-case letters ('a'-'z') as possible,
 # in which each letter occurs an equal number of times.
+import string
 
 def solution(N):
     # Validate N is an innteger, and is bwtween 1 and 200,000
     if type(N) == int and 1 <= N <= 200000:
-        print("truthy")
+        # use string module to get a string of all alphabets in lowercase
+        lowercase_alphabet_string = string.ascii_lowercase
+        
     else:
         raise ValueError("'N' has to be an integer between 1 and 200000")
     
