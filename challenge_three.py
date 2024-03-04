3beff196c6df179997a2bca061135e2dc31e77ca
John <johntaykimani@gmail.com>
2024-03-04 10:41:56 +0300

    feat: Define a function that takes input N.

---
+++ challenge_three.py
@@ -0,0 +1,12 @@
+# Write a function solution that, given an integer N, returns a string of length N containing
+# as many different lower-case letters ('a'-'z') as possible,
+# in which each letter occurs an equal number of times.
+
+def solution(N):
+    pass
+
+# Test Cases
+print(solution(3))
+print(solution(5))
+print(solution(30))
+print(solution("A string"))
