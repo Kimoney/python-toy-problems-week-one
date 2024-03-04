4582407c54b42e901af4ac0f1d7386e0c79842e8
John <johntaykimani@gmail.com>
2024-03-04 09:57:20 +0300

    feat: Logic to return max sum for two numbers whose digits have an equal sum.

--- challenge_two.py
+++ challenge_two.py
@@ -15,11 +15,24 @@ def solution(A):
             second_digit = round(digits[0], 2) * 10
             first_digit = digits[1]
             sum_of_digits.append(int(second_digit+first_digit))
-        print(sum_of_digits)
     
     # Validate whether we have two numbers whose digits have an equal sum else return -1
         if len(sum_of_digits) != len(set(sum_of_digits)):
-            print("We Can Process this Array")
+
+    # Return max sum for two numbers whose digits have an equal sum
+           # Function to calculate the sum of digits of a number 
+            def add_digits(num):
+                return sum(int(digit) for digit in str(num))
+
+            max_sum = 0
+            
+            for i in range(N):
+                for n in range(i+1, len(A)):
+                    sum_i = add_digits(A[i])
+                    sum_n = add_digits(A[n])
+                    if sum_i == sum_n:
+                        max_sum = max(max_sum, A[i] + A[n])
+            return max_sum
         else:
             return -1
 
@@ -27,9 +40,7 @@ def solution(A):
         raise ValueError("A has to be a list.")
 
 # Test Cases
-# print(dir(int))
 print(solution([51, 71, 17, 42]))
 print(solution([42, 33, 60]))
 print(solution([51, 32, 43]))
-# print(solution("Just a string"))
-# print(solution(1234))
+print(solution("Just a string"))
